ad4e6abb677c1b825edf81b687ebf128ae1d7881
Artem <a.savelev2002@gmail.com>
2023-03-02 21:51:32 +0400

    first solution

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using PowerCollections;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            PowerCollections.Stack<int> stack = new PowerCollections.Stack<int>();  
+            Console.WriteLine(stack.Capacity);
+           
+        }
+    }
+}
---
+++ PowerCollections/PowerCollections/build_and_publish_nuget.cmd
@@ -0,0 +1,4 @@
+dotnet pack
+dotnet nuget push bin\Debug\Savelev2002.PowerCollections.0.0.4-beta.nupkg  --api-key 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 --source github_S --skip-duplicate
+Pause
+
